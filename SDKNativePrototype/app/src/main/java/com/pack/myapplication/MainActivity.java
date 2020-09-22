db770e6ed137241e7b1e42c326d5dc92649376d5
Facundo Ezequiel Gutierrez Uran <facundo.gutierrez@fing.edu.uy>
2020-09-22 02:19:05 -0300

    Permite solicitar el código

--- SDKNativePrototype/app/src/main/java/com/pack/myapplication/MainActivity.java
+++ SDKNativePrototype/app/src/main/java/com/pack/myapplication/MainActivity.java
@@ -2,8 +2,12 @@ package com.pack.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                click_btn_login("https://auth-testing.iduruguay.gub.uy/oidc/v1/authorize?scope=openid+email&response_type=code&client_id=894329&redirect_uri=sdkIdU.testing://auth");
+            }
+        });
+
+        //El tema de la uri cuando se hace el redirect. En el manifest esta configurado el deep linking
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+        //Uri uri = getIntent().getData();
+        if(data!=null){
+            String code = data.getQueryParameter("code");
+            if(code!=null) // el usuario final acepto
+                Toast.makeText(this, "Tu code es:"+code,Toast.LENGTH_LONG).show();
+            else // el usuario final cancelo
+                Toast.makeText(this, "No le diste a aceptar",Toast.LENGTH_LONG).show();
+        }
     }
+
+    //Para que se abra el navegador
+    public void click_btn_login(String uri){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(uri));
+        startActivity(intent);
+    }
+
 }
